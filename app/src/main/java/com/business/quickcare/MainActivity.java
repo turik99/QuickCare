6fc00d226caeb3364aa11520020bebf61014e4e7
Eric <eric.zaba@gmail.com>
2019-05-20 01:21:57 -0500

    added views and stuff

--- app/src/main/java/com/business/quickcare/MainActivity.java
+++ app/src/main/java/com/business/quickcare/MainActivity.java
@@ -9,5 +9,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //OK, a bit of context, right now I'm just trying to get something 'off the ground' that will
+        //function as intended, so everything written thus far is subjecto change, and the user flow
+        //should be more elegant than I'm about to write it.
+        //Also research android lifecylce that's the like the only important thing you need to know about
+        //android programming.
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/business/quickcare/providerResultsActivity.java
@@ -0,0 +1,16 @@
+package com.business.quickcare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class providerResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provider_results);
+
+
+
+    }
+}

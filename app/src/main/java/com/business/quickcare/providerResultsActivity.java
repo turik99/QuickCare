ec46358176cb4090a4794a79b1d0ae07357604ba
Eric <eric.zaba@gmail.com>
2019-05-25 00:06:43 -0500

    I haven't changed anything on the UI, but it does grab data from firebase successfully now.

--- app/src/main/java/com/business/quickcare/providerResultsActivity.java
+++ app/src/main/java/com/business/quickcare/providerResultsActivity.java
@@ -1,9 +1,17 @@
 package com.business.quickcare;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class ProviderResultsActivity extends AppCompatActivity {
 
@@ -14,6 +22,43 @@ public class ProviderResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provider_results);
+
+
+
+
+
+
+
+
+        //Before we starting messing with the UI elements like the recycler view and stuff, we're going to get the Firebase data
+        //Remember that we will eventually need to be sorting this data w/ ui elements like the spinners
+        //and also sorting the data by location naturally
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("healthcareproviders")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    private String TAG = "FireBaseFireStore";
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
+
+
+
+
+
+
 
         String[] myDataset = {"Advocate", "Eric Hospitals", "Riley Hospitals", "Dylan Hospitals",
                 "Advocate", "Eric Hospitals", "Riley Hospitals", "Dylan Hospitals",

96019f39729850bf7e875182eed3e913706bd86a
Eric <eric.zaba@gmail.com>
2019-06-09 01:58:33 -0500

    Provider page is getting more done. connects to database now.

--- app/src/main/java/com/business/quickcare/ProviderPageActivity.java
+++ app/src/main/java/com/business/quickcare/ProviderPageActivity.java
@@ -1,11 +1,19 @@
 package com.business.quickcare;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.model.Document;
 
 public class ProviderPageActivity extends AppCompatActivity {
 
@@ -20,10 +28,35 @@ public class ProviderPageActivity extends AppCompatActivity {
         this.documentId = getIntent().getStringExtra("documentId");
 
 
+        final TextView providerDetailsName = findViewById(R.id.providerDetailsName);
+        final TextView providerDetailsAddress = findViewById(R.id.providerDetailsAddress);
+        final TextView practiceSummaryDetails = findViewById(R.id.practiceSummaryDetails);
+        final TextView ratingDetailsText = findViewById(R.id.ratingDetailsText);
+        final TextView operatingTime = findViewById(R.id.yearsOperatingDetails);
+        final TextView pricingSummary = findViewById(R.id.priceSummary);
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("healthcareproviders").document(documentId);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("HP Page Firebase", "DocumentSnapshot data: " + document.getData());
+                        providerDetailsName.setText(document.getString("name"));
+                        ratingDetailsText.setText(document.getString("rating"));
+                    } else {
+                        Log.d("HP Page Firebase", "No such document");
+                    }
+                } else {
+                    Log.d("HP Page Firebase", "get failed with ", task.getException());
+                }
+            }
+        });
 
 
     }

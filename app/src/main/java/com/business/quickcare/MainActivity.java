1a1a40c74288d7745e4e5a1b110cb9f4a41cf943
Eric <eric.zaba@gmail.com>
2019-07-14 12:24:23 -0500

    working on requesting location from user when button pressed now

--- app/src/main/java/com/business/quickcare/MainActivity.java
+++ app/src/main/java/com/business/quickcare/MainActivity.java
@@ -1,15 +1,28 @@
 package com.business.quickcare;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.type.LatLng;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Location location;
+    private LatLng latLng;
+    private FusedLocationProviderClient providerClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +40,45 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+    public void getLocation(View view)
+    {
+
+        int locationRequestCode = 42069;
+        // check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // reuqest for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, locationRequestCode);
+
+        } else {
+            // already permission granted
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 42069: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                    providerClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+                }
+            }
+        }
     }
 
 
-
-    public void findProviders(View view)
+                    public void findProviders(View view)
     {
         //This method is called when the user clicks the find providers button, it's defined in the
         //android:onclick attribute of the xml.
--- app/src/main/java/com/business/quickcare/ProviderResultsActivity.java
+++ app/src/main/java/com/business/quickcare/ProviderResultsActivity.java
@@ -52,7 +52,7 @@ public class ProviderResultsActivity extends AppCompatActivity {
 
 
         //This is instantiating an arraylist of the quickareprovider class, see that class for details on what data is stored here.
-        final ArrayList<QuickCareProvider> listOfProviders = new ArrayList<>(10);
+        final ArrayList<QuickCareProvider> listOfProviders = new ArrayList<>();
 
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -60,58 +60,37 @@ public class ProviderResultsActivity extends AppCompatActivity {
 
 
         GeoFirestore geoFirestore = new GeoFirestore(reference);
-        GeoQuery geoQuery = geoFirestore.queryAtLocation(new GeoPoint(47.82, -81.22), 0.6);
+        GeoQuery geoQuery = geoFirestore.queryAtLocation(new GeoPoint(19.5272595, 49.9195225), 8587);
 
 
-        ArrayList<String[]> documentsList = new ArrayList<String[]>();
 
-
-        geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+        geoQuery.addGeoQueryDataEventListener(new GeoQueryDataEventListener() {
             @Override
-            public void onKeyEntered(String s, GeoPoint geoPoint) {
-                //This is the real 'key' (no pun intended) to the library, on key entered menas that there is a document with a 'key' that's the id inside your Query
-                //which is what we want, we want to see those documents that are within the radius of our user.
-                //I am not a programmer so I found the logic funny here, but this method does not produce a list of keys that are in the radius, rather it produces one, and then it is called again and again
-                //if there is another id that is there.
-                String[] strings = new String[3];
-                strings[0] = s;
-                strings[1] = String.valueOf(geoPoint.getLatitude());
-                strings[2] = String.valueOf(geoPoint.getLongitude());
+            public void onDocumentEntered(DocumentSnapshot doc, GeoPoint geoPoint) {
+                Log.v("GeoFire", "Key Entered");
 
-                documentsList.add(strings);
+                listOfProviders.add(new QuickCareProvider(doc.getString("name"), doc.getString("address"), doc.getString("rating"), doc.getId(), geoPoint));
             }
 
             @Override
-            public void onKeyExited(String s) {
+            public void onDocumentExited(DocumentSnapshot documentSnapshot) {
 
             }
 
             @Override
-            public void onKeyMoved(String s, GeoPoint geoPoint) {
+            public void onDocumentMoved(DocumentSnapshot documentSnapshot, GeoPoint geoPoint) {
+
+            }
+
+            @Override
+            public void onDocumentChanged(DocumentSnapshot documentSnapshot, GeoPoint geoPoint) {
 
             }
 
             @Override
             public void onGeoQueryReady() {
-                for (String[] s: documentsList)
-                {
-                    Log.v("GeoFire Query", s[0]);
 
-                    double lat = Double.valueOf(s[1]);
-                    double lng = Double.valueOf(s[2]);
-
-                    DocumentReference docRef = reference.document(s[0]);
-                    docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                        @Override
-                        public void onSuccess(DocumentSnapshot doc) {
-                            //this is called when the document is gotten successfully as it should be yeet
-                            listOfProviders.add(new QuickCareProvider(doc.getString("name"),
-                                    doc.getString("address"), doc.getString("rating"), doc.getId(), new GeoPoint(lat, lng)));
-
-
-                        }
-                    });
-                }
+                Log.v("GeoFire", "Query Ready");
 
 
                 //Instantiating a recyclerview, which is really a list view. The following code is mostly
@@ -130,8 +109,6 @@ public class ProviderResultsActivity extends AppCompatActivity {
                 mAdapter = new MyAdapter(listOfProviders, ProviderResultsActivity.this);
                 providerList.setAdapter(mAdapter);
 
-
-
             }
 
             @Override
@@ -141,86 +118,11 @@ public class ProviderResultsActivity extends AppCompatActivity {
         });
 
 
+
         Log.v("ProviderDataSetTest", listOfProviders.toString());
 
 
 
-
-
-        /*Whoah THere Bucko! This code is only for testing! We're sorting by distance and stuff
-        now, so be sure to not have this around anymore soon.
-         */
-
-
-//        FirebaseFirestore dbTest = FirebaseFirestore.getInstance();
-//        db.collection("healthcareproviders")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    private String TAG = "FireBaseFireStore";
-//
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                Log.d(TAG, document.getId() + " => " + document.getData());
-//                                listOfProviders.add(new QuickCareProvider(document.getString("name"),
-//                                        String.valueOf(document.get("ZipCode")),
-//                                        document.getString("rating"), document.getId(), new GeoPoint(-87.7346, 41.7218)));
-//                            }
-//                        } else {
-//                            Log.w(TAG, "Error getting documents.", task.getException());
-//                        }
-//
-//
-//
-//                        //Instantiating a recyclerview, which is really a list view. The following code is mostly
-//                        //boiler plate from the Android Developers website.
-//                        RecyclerView providerList = findViewById(R.id.providerList);
-//
-//                        // use this setting to improve performance if you know that changes
-//                        // in content do not change the layout size of the RecyclerView
-//                        providerList.setHasFixedSize(true);
-//
-//                        // use a linear layout manager
-//                        layoutManager = new LinearLayoutManager(getApplicationContext());
-//                        providerList.setLayoutManager(layoutManager);
-//
-//                        // specify an adapter (see also next example)
-//                        mAdapter = new MyAdapter(listOfProviders, ProviderResultsActivity.this);
-//                        providerList.setAdapter(mAdapter);
-//
-//
-//
-//                    }
-//                });
-//
-//
-//        Log.v("ProviderDataSetTest", listOfProviders.toString());
-
-
-        /*End of testing code!!!!!!!!
-        * */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
-    public void doStuffWithDocumentIds(ArrayList<String> ids)
-    {
 
-    }
 }
